bfac6378cb99dd615561f78be7cf35257a5c9ccc
bleck09 <blady12345tkm@gmail.com>
2025-11-04 20:21:39 -0400

    cambiando rustas con hast

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom"; // 👈 cambia aquí
 import Error404 from "../components/pages/error404/Error404";
 import Products from '../components/pages/productos/Products';
 import App from '../components/templates/App';
 import Home from '../components/pages/home/Home';
 import Login from '../components/pages/login/Login';
 
-const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <App/>,
-        errorElement: <Error404/>,
-        children:[
-            {
-                index: true,
-                element: <Home/>,
-            },
-            {
-                path:"/productos",
-                element: <Products/>,
-            }
-        ]
-    },
-    {
-        path: "/login",
-        element: <Login/>
-    }
-])
+const router = createHashRouter([  // 👈 también aquí
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/productos",
+        element: <Products />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
-export default router
+export default router;
